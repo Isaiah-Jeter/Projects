9a696bdcc7a8c3d774bef8bc0cd371db31d4fe4b
Isaiah-Jeter <121069550+Isaiah-Jeter@users.noreply.github.com>
2022-12-20 14:40:27 -0800

    Update and rename Recursive Permutations/RecPermTester.java to Recursive Permutations (Completed)/RecPermTester.java

--- Recursive Permutations (Completed)/RecPermTester.java
+++ Recursive Permutations (Completed)/RecPermTester.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class RecPermTester {
 	public static void main (String [] args) {
 		System.out.println (RecursivePermutations.recursivePermutations ("cat"));
-		System.out.println (RecursivePermutations.recursivePermutations ("dog"));		System.out.println (RecursivePermutations.recursivePermutations ("cat"));
+		System.out.println (RecursivePermutations.recursivePermutations ("dog"));		
+		System.out.println (RecursivePermutations.recursivePermutations ("cat"));
 		System.out.println (RecursivePermutations.recursivePermutations ("hi"));
 		System.out.println (RecursivePermutations.recursivePermutations ("ok"));
 		System.out.println (RecursivePermutations.recursivePermutations ("trip"));
